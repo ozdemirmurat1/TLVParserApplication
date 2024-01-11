71b1706ac5e357d79687628cc32474e1d26d08fe
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2024-01-11 14:20:21 +0300

    Add project files.

---
+++ TLVParserApp/Program.cs
@@ -0,0 +1,141 @@
+﻿namespace TLVParserApp
+{
+    // UYGULAMA .NET8 e YÜKSELTİLDİ....
+    // GETLENGTH GETTAG METOTLARINA TRY EXCEPTİON EKLENECEK.....
+
+    internal class Program
+    {
+        static List<TagList> tagListValues = new List<TagList>();
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter hexadecimal value: ");
+            var readHexadecimalValue=Console.ReadLine()!.Replace(" ","");
+
+            RecursiveLoopTagValues(readHexadecimalValue);
+
+            foreach (var tag in tagListValues)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"Tag: {tag.TagValue}, Length: {tag.HexadecimalTagValue} , TagName: {tag.TagName}, Value: {tag.DecimalTagValue}");
+                Console.ResetColor();
+            }
+        }
+
+        // LENGTH'I HEXADECİMAL FORMAT TA DA YAZDIR. PROGRAM DECİMAL YAZDIRIYOR
+        static void RecursiveLoopTagValues(string h)
+        {
+            int index = 0;
+            while (index < h.Length)
+            {
+                string tag = GetTag(h, ref index);
+                int length=(int)GetLength(h, ref index);
+                string hexLengthValue=DecimalToHex(length);
+                string value = h.Substring(index, length * 2);
+                index += length * 2;
+
+                tagListValues.Add(new TagList
+                {
+                    TagName = EMVFieldNames.emvTagNames
+                            .FirstOrDefault(x => x.Key.Equals(tag))
+                            .Value ?? "Unknown Tag",
+                    TagValue = tag,
+                    TagLentghValue = length.ToString(),
+                    HexadecimalTagValue = hexLengthValue,
+                    DecimalTagValue = value,
+                });
+
+                if (IsConstructedTag(tag))
+                {
+                    RecursiveLoopTagValues(value);
+                }
+            }
+        }
+
+        // void RecursiveLoopTagValues metodundaki index değişkeni GetTag metoduna  ref anahtar sözcüğüyle değerini atar.
+        static string GetTag(string h,ref int index)
+        {
+            try
+            {
+                string tag = h.Substring(index, 2);
+                index += 2;
+
+
+                // 0x1F 00011111 bit decimal 31
+
+                if ((Convert.ToInt32(tag, 16) & 0x1F) == 0x1F)
+                {
+                    tag += h.Substring(index, 2);
+                    index += 2;
+                }
+
+                return tag;
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+            
+        }
+
+        // void RecursiveLoopTagValues metodundaki index değişkeni GetLength metoduna  ref anahtar sözcüğüyle değerini atar.
+        static object GetLength(string h,ref int index)
+        {
+            try
+            {
+                int length = Convert.ToInt32(h.Substring(index, 2), 16);
+                index += 2;
+
+                // 0x80 hexadecimal sayısı, ikili tabanda 10000000'a eşittir.(128 decimal) en sol bit i kontrol eder
+                // 0x7F  01111111
+
+                if ((length & 0x80) != 0)
+                {
+                    int numberOfLengthBytes = length & 0x7F;
+                    length = Convert.ToInt32(h.Substring(index, numberOfLengthBytes * 2), 16);
+                    index += numberOfLengthBytes * 2;
+                }
+
+                return length;
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+            
+        }
+
+        static bool IsConstructedTag(string tag)
+        {
+            try
+            {
+                // Constructed kontrolü yapılır. Bit 6 
+                //0x20(yani 32 ondalık veya 00100000 ikilik)
+
+                int firstByte = Convert.ToInt32(tag.Length > 0 ? tag.Substring(0, 2) : "00", 16);
+                bool control= (firstByte & 0x20) == 0x20;
+                return control;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        static string DecimalToHex(int decimalValue)
+        {
+            try
+            {
+                return Convert.ToString(decimalValue, 16).ToUpper();
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+            
+        }
+
+
+
+    }
+}
---
+++ TLVParserApp/TagList.cs
@@ -0,0 +1,11 @@
+﻿namespace TLVParserApp
+{
+    public class TagList
+    {
+        public string? TagName { get; set; }
+        public string? TagValue { get; set; }
+        public string? TagLentghValue { get; set; }
+        public string? DecimalTagValue { get; set; }
+        public string? HexadecimalTagValue { get; set; }
+    }
+}
